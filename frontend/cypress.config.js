9410145bbf3b8fd33dbd42ad4fb00db404589f76
zionfinale-cloud <zls4e@msn.com>
2025-12-16 04:54:11 -0600

    chore(cypress): Añade archivo de configuración para especificar la ruta de las pruebas

---
+++ frontend/cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+    // CRÍTICO: Le decimos a Cypress que busque las pruebas en esta carpeta
+    specPattern: 'cypress/e2e/**/*.cy.js',
+  },
+});
